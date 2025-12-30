0967a6b14cfbcfc34bf1a231ad6534a3fea5c191
solehasiddique <solehasiddique07@gmail.com>
2025-12-30 21:39:59 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
-
+const articleRoutes = require("./routes/articleRoutes");
+app.use("/api/articles", articleRoutes);
 app.use(express.json());
+
+const scrapeRoutes = require("./routes/scrapeRoutes");
+app.use("/api", scrapeRoutes);
+
+
 app.get("/", (req, res) => {
   res.send("BeyondChats API is running");
 });
-
 
 module.exports = app;
